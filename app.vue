187c8d627847b2ec8faaca7806579d20252a50f8
Vitaliy Didyk <79630556+MrTartuf0@users.noreply.github.com>
2023-10-16 21:35:00 +0200

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Rosticceria I Mori
+  </h1>
+  <img class="w-auto" src="~/assets/img/rosticceria_mori.jpeg">
+  <img class="w-12" src="~/assets/icons/plus.svg">
+  <img class="w-12" src="~/assets/icons/info.svg">
+  <img class="w-12" src="~/assets/icons/arrow.svg">
+  <img class="w-12" src="~/assets/icons/shopping-cart.svg">
+  <img class="w-12" src="~/assets/icons/minus.svg">
+</template>
